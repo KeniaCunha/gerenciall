73ace6278922ba396065a65256e9619b6f5d8541
KeniaCunha <keniacunha100@gmail.com>
2021-11-27 21:25:32 -0300

    test

--- facebook.script.js
+++ facebook.script.js
@@ -7,33 +7,19 @@ function statusChangeCallback(response) {  // Chamado com os resultados de FB.ge
 }
 
 
-function login() {
-  FB.login(function(response) {
-    if (response.status === 'connected') {
-      document.getElementById('btnFacebook').innerHTML = 'Conectado';
-      document.getElementById('btnFacebook').style.background = 'green';
-
-      const userId = response.authResponse.userID;
-      document.getElementById('img-usuario').innerHTML = '<img src="https://graph.facebook.com/' + userId + '/picture" alt="' + response.name + '"/>';
-      //Conectado à sua página da web e Facebook.
-    } else {
-      // a pessoa não está logada em sua página da web ou não podemos saber. 
-    }
-  }, { scope: 'publish_to_groups,pages_read_engagement,pages_manage_posts', return_scopes: true });
-}
 
 var botaoPublicarPost = document.getElementById('botaoPublicar');
 
 
 
 function checkLoginState() {               // Chamado quando uma pessoa termina com o botão de login.
-  FB.getLoginStatus(function(response) {   // Veja o manipulador de onlogin 
+  FB.getLoginStatus(function (response) {   // Veja o manipulador de onlogin 
     statusChangeCallback(response);
   });
 }
 
 
-window.fbAsyncInit=function() {
+window.fbAsyncInit = function () {
   FB.init({
     appId: '4316391405155414',
     cookie: true,                     //  Habilite os cookies para permitir que o servidor acesse a sessão
@@ -43,21 +29,38 @@ window.fbAsyncInit=function() {
 
 
 
-  botaoPublicarPost.addEventListener('click', function() {
+  botaoPublicarPost.addEventListener('click', function () {
     publishPost()
   })
-  
+
   function publishPost() {
     var body = 'Lendo documentação JS SDK ';
-    FB.api('/me/feed', 'post', { message: body }, function(response) {
+    FB.api('/me/feed', 'post', { message: body }, function (response) {
       console.log(response);
       if (!response || response.error) {
-        
+
         alert('Ocorreu um erro');
       } else {
         alert('Post ID: ' + response.id);
       }
     });
+  }
+
+
+  function login() {
+    FB.login(function (response) {
+      console.log(response)
+      if (response.status === 'connected') {
+        document.getElementById('btnFacebook').innerHTML = 'Conectado';
+        document.getElementById('btnFacebook').style.background = 'green';
+
+        const userId = response.authResponse.userID;
+        document.getElementById('img-usuario').innerHTML = '<img src="https://graph.facebook.com/' + userId + '/picture" alt="' + response.name + '"/>';
+        //Conectado à sua página da web e Facebook.
+      } else {
+        // a pessoa não está logada em sua página da web ou não podemos saber. 
+      }
+    }, { scope: 'publish_to_groups,pages_read_engagement,pages_manage_posts', return_scopes: true });
   }
 
   // FB.getLoginStatus(function(response) {   // Chamado depois que o JS SDK foi inicializado.
@@ -67,7 +70,7 @@ window.fbAsyncInit=function() {
 
 function testAPI() {                      // Testando Graph API após o login.  Veja statusChangeCallback() para saber quando esta chamada é feita..
   console.log('Bem-vindo! Buscando suas informações ....');
-  FB.api('/me', function(response) {
+  FB.api('/me', function (response) {
     // console.log('Login bem-sucedido para: ' + response.name);
     // document.getElementById('status').innerHTML =
     //   'Obrigado por fazer login, ' + response.name + '!';
